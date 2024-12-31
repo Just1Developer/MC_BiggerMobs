4604d17deb4855f88f9ec247be0cb007b7b1c7c8
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-12-31 11:52:59 +0100

    Initial commit

---
+++ src/main/java/net/justonedev/mc/biggerCreakings/BiggerCreakings.java
@@ -0,0 +1,17 @@
+package net.justonedev.mc.biggerCreakings;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BiggerCreakings extends JavaPlugin im {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
